b69de9df2d446c023307c511fb02c3a2009a967d
iau676 <ibrahimuysal676@gmail.com>
2022-12-20 22:12:12 +0300

    fix bug

--- Word Bank/Model/Player.swift
+++ Word Bank/Model/Player.swift
@@ -27,7 +27,7 @@ class Player {
     
     func playMP3(_ soundName: String) {
         if UserDefaults.standard.integer(forKey: "playAppSound") == 0 {
-            let url = Bundle.main.url(forResource: "/Sounds/\(soundName)", withExtension: "mp3")
+            let url = Bundle.main.url(forResource: "\(soundName)", withExtension: "mp3")
             playerMP3 = try! AVAudioPlayer(contentsOf: url!)
             do {
                 try AVAudioSession.sharedInstance().setCategory(AVAudioSession.Category.playback)
