6f5445f6c9ab8c7de4011f8d12715104c5877264
iau676 <ibrahimuysal676@gmail.com>
2024-08-17 03:48:18 +0300

    add TaskView

--- Word Bank/Controller/DailyController.swift
+++ Word Bank/Controller/DailyController.swift
@@ -8,23 +8,9 @@
 import UIKit
 
 class DailyController: UIViewController {
-        
-    private let secondView = UIView()
     
-    private let taskOneButton = UIButton()
-    private let taskOneButtonBlueLayer = UIButton()
-    private let taskOneButtonRavenLayer = UIButton()
+    //MARK: - Properties
     
-    private let taskTwoButton = UIButton()
-    private let taskTwoButtonBlueLayer = UIButton()
-    private let taskTwoButtonRavenLayer = UIButton()
-    
-    private let taskThreeButton = UIButton()
-    private let taskThreeButtonBlueLayer = UIButton()
-    private let taskThreeButtonRavenLayer = UIButton()
-
-    private let prizeButton = UIButton()
-
     private var wordBrain = WordBrain()
     private var itemArray: [Item] { return wordBrain.itemArray }
     private var exerciseArray: [Exercise] { return wordBrain.exerciseArray }
@@ -33,14 +19,58 @@ class DailyController: UIViewController {
         let int = wordBrain.getExerciseCountToday(for: ExerciseFormat.test)
         return int > 10 ? 10 : int
     }()
+    
     private lazy var writingExerciseCount: Int = {
         let int = wordBrain.getExerciseCountToday(for: ExerciseFormat.writing)
         return int > 10 ? 10 : int
     }()
+    
     private lazy var listeningExerciseCount: Int = {
         let int = wordBrain.getExerciseCountToday(for: ExerciseFormat.listening)
         return int > 10 ? 10 : int
     }()
+    
+    private let secondView: UIView = {
+       let view = UIView()
+        view.backgroundColor = Colors.cellRight
+        view.setViewCornerRadius(10)
+        return view
+    }()
+    
+    private lazy var testTaskView: UIView = {
+       let view = TaskView(title: "Complete 10 Test Exercise", exerciseCount: testExerciseCount)
+        view.isUserInteractionEnabled = true
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(testTaskPressed))
+        view.addGestureRecognizer(tapGestureRecognizer)
+        return view
+    }()
+    
+    private lazy var writingTaskView: UIView = {
+       let view = TaskView(title: "Complete 10 Writing Exercise", exerciseCount: writingExerciseCount)
+        view.isUserInteractionEnabled = true
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(writingTaskPressed))
+        view.addGestureRecognizer(tapGestureRecognizer)
+        return view
+    }()
+    
+    private lazy var listeningTaskView: UIView = {
+       let view = TaskView(title: "Complete 10 Listening Exercise", exerciseCount: listeningExerciseCount)
+        view.isUserInteractionEnabled = true
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(listeningTaskPressed))
+        view.addGestureRecognizer(tapGestureRecognizer)
+        return view
+    }()
+    
+    private lazy var prizeButton: UIButton = {
+        let button = UIButton()
+        button.setImage(image: Images.wheel_prize_present, width: 128, height: 128)
+        button.backgroundColor = .clear
+        button.isEnabled = false
+        button.addTarget(self, action: #selector(prizeButtonPressed), for: .primaryActionTriggered)
+        return button
+    }()
+    
+    //MARK: - Lifecycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -48,32 +78,34 @@ class DailyController: UIViewController {
         wordBrain.loadExerciseArray()
         wordBrain.findExercisesCompletedToday()
 
-        style()
-        layout()
-        updateButtons()
+        configureUI()
+        configurePrizeButton()
     }
     
     //MARK: - Selectors
     
-    @objc private func taskOneButtonPressed(){
+    @objc private func testTaskPressed() {
+        testTaskView.bounce()
         let controller = TestController(exerciseType: ExerciseType.normal,
                                         exerciseFormat: ExerciseFormat.test)
         checkWordCountAndNavigate(controller: controller)
     }
     
-    @objc private func taskTwoButtonPressed(){
+    @objc private func writingTaskPressed() {
+        writingTaskView.bounce()
         let controller = WritingController(exerciseType: ExerciseType.normal,
                                            exerciseFormat: ExerciseFormat.writing)
         checkWordCountAndNavigate(controller: controller)
     }
     
-    @objc private func taskThreeButtonPressed(){
+    @objc private func listeningTaskPressed() {
+        listeningTaskView.bounce()
         let controller = ListeningController(exerciseType: ExerciseType.normal,
                                              exerciseFormat: ExerciseFormat.listening)
         checkWordCountAndNavigate(controller: controller)
     }
     
-    @objc private func prizeButtonPressed(){
+    @objc private func prizeButtonPressed() {
         prizeButton.bounce()
         DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 0.1){
             self.navigationController?.pushViewController(WheelController(), animated: true)
@@ -82,97 +114,9 @@ class DailyController: UIViewController {
     
     //MARK: - Helpers
     
-    private func checkWordCountAndNavigate(controller: UIViewController){
-        let wordCount = itemArray.count
-        
-        if wordCount < 2 {
-            showAlert(title: "Minimum two words required", message: "") { _ in
-                self.navigationController?.popToRootViewController(animated: true)
-            }
-        } else {
-            navigationController?.pushViewController(controller, animated: true)
-        }
-    }
-    
-    private func updateButtons(){
-        if testExerciseCount >= 10 && writingExerciseCount >= 10 && listeningExerciseCount >= 10 {
-            if UserDefault.userGotDailyPrize.getString() != wordBrain.getTodayDate() {
-                prizeButton.isEnabled = true
-            }
-        }
-        
-        if testExerciseCount >= 10 {
-            taskOneButton.setImageWithRenderingMode(image: Images.check, width: 25, height: 25, color: .white)
-        }
-        
-        if writingExerciseCount >= 10 {
-            taskTwoButton.setImageWithRenderingMode(image: Images.check, width: 25, height: 25, color: .white)
-        }
-        
-        if listeningExerciseCount >= 10 {
-            taskThreeButton.setImageWithRenderingMode(image: Images.check, width: 25, height: 25, color: .white)
-        }
-    }
-    
-    private func configureLayerButton(_ button: UIButton, _ color: UIColor?) {
-        button.setHeight(60)
-        button.setButtonCornerRadius(8)
-        button.backgroundColor = color
-    }
-    
-    private func configureButton(_ button: UIButton, _ text: String) {
-        button.setTitle(text, for: .normal)
-        button.titleLabel?.font = UIFont(name: Fonts.AvenirNextDemiBold, size: 15)
-    }
-}
-
-//MARK: - Layout
-
-extension DailyController {
-    
-    private func style(){
+    private func configureUI() {
         title = "Daily"
         view.backgroundColor = Colors.cellLeft
-        
-        secondView.backgroundColor = Colors.cellRight
-        secondView.setViewCornerRadius(10)
-        
-        configureButton(taskOneButton, "Complete 10 Test Exercise")
-        configureButton(taskTwoButton, "Complete 10 Writing Exercise")
-        configureButton(taskThreeButton, "Complete 10 Listening Exercise")
-        
-        taskOneButton.setImageWithRenderingMode(image: Images.whiteCircle, width: 25, height: 25, color: .white)
-        taskTwoButton.setImageWithRenderingMode(image: Images.whiteCircle, width: 25, height: 25, color: .white)
-        taskThreeButton.setImageWithRenderingMode(image: Images.whiteCircle, width: 25, height: 25, color: .white)
-        
-        taskOneButton.moveImageTitleLeft()
-        taskTwoButton.moveImageTitleLeft()
-        taskThreeButton.moveImageTitleLeft()
-        
-        taskOneButton.addTarget(self, action: #selector(taskOneButtonPressed), for: .primaryActionTriggered)
-        taskTwoButton.addTarget(self, action: #selector(taskTwoButtonPressed), for: .primaryActionTriggered)
-        taskThreeButton.addTarget(self, action: #selector(taskThreeButtonPressed), for: .primaryActionTriggered)
-        
-        configureLayerButton(taskOneButtonBlueLayer, Colors.blue)
-        configureLayerButton(taskOneButtonRavenLayer, Colors.raven)
-        
-        configureLayerButton(taskTwoButtonBlueLayer, Colors.blue)
-        configureLayerButton(taskTwoButtonRavenLayer, Colors.raven)
-        
-        configureLayerButton(taskThreeButtonBlueLayer, Colors.blue)
-        configureLayerButton(taskThreeButtonRavenLayer, Colors.raven)
-        
-        prizeButton.setImage(image: Images.wheel_prize_present, width: 128, height: 128)
-        prizeButton.backgroundColor = .clear
-        prizeButton.isEnabled = false
-        prizeButton.addTarget(self, action: #selector(prizeButtonPressed), for: .primaryActionTriggered)
-    }
-    
-    private func layout(){
-        let taskButtonWidth = view.bounds.width-64-32
-        let taskOneBlueLayerWidth = (taskButtonWidth/10)*CGFloat(testExerciseCount)
-        let taskTwoBlueLayerWidth = (taskButtonWidth/10)*CGFloat(writingExerciseCount)
-        let taskThreeBlueLayerWidth = (taskButtonWidth/10)*CGFloat(listeningExerciseCount)
         
         view.addSubview(secondView)
         secondView.anchor(left: view.leftAnchor, right: view.rightAnchor,
@@ -180,43 +124,42 @@ extension DailyController {
         secondView.centerY(inView: view)
         secondView.setHeight(388) //16*5 + 60*3 + 128
         
-        //raven layer
-        let stackViewRaven = UIStackView(arrangedSubviews: [taskOneButtonRavenLayer,
-                                                            taskTwoButtonRavenLayer,
-                                                            taskThreeButtonRavenLayer])
-        stackViewRaven.distribution = .fillEqually
-        stackViewRaven.axis = .vertical
-        stackViewRaven.spacing = 16
+        let stack = UIStackView(arrangedSubviews: [testTaskView, writingTaskView, listeningTaskView])
+        stack.distribution = .fillEqually
+        stack.axis = .vertical
+        stack.spacing = 16
         
-        secondView.addSubview(stackViewRaven)
-        stackViewRaven.centerX(inView: view)
-        stackViewRaven.anchor(top: secondView.topAnchor, left: secondView.leftAnchor, right: secondView.rightAnchor,
-                              paddingTop: 16, paddingLeft: 16, paddingRight: 16)
-  
-        //blue layer
-        taskOneButtonRavenLayer.addSubview(taskOneButtonBlueLayer)
-        taskOneButtonBlueLayer.setWidth(taskOneBlueLayerWidth)
-        
-        taskTwoButtonRavenLayer.addSubview(taskTwoButtonBlueLayer)
-        taskTwoButtonBlueLayer.setWidth(taskTwoBlueLayerWidth)
+        testTaskView.setHeight(60)
+        secondView.addSubview(stack)
+        stack.centerX(inView: view)
+        stack.anchor(top: secondView.topAnchor, left: secondView.leftAnchor, right: secondView.rightAnchor,
+                     paddingTop: 16, paddingLeft: 16, paddingRight: 16)
 
-        taskThreeButtonRavenLayer.addSubview(taskThreeButtonBlueLayer)
-        taskThreeButtonBlueLayer.setWidth(taskThreeBlueLayerWidth)
-        
-        //button
-        taskOneButtonRavenLayer.addSubview(taskOneButton)
-        taskOneButton.fillSuperview()
-        
-        taskTwoButtonRavenLayer.addSubview(taskTwoButton)
-        taskTwoButton.fillSuperview()
-        
-        taskThreeButtonRavenLayer.addSubview(taskThreeButton)
-        taskThreeButton.fillSuperview()
-
-        //prize button
         secondView.addSubview(prizeButton)
         prizeButton.centerX(inView: secondView)
         prizeButton.setDimensions(width: 128, height: 128)
-        prizeButton.anchor(top: stackViewRaven.bottomAnchor, paddingTop: 16)
+        prizeButton.anchor(top: stack.bottomAnchor, paddingTop: 16)
+    }
+    
+    private func configurePrizeButton() {
+        if testExerciseCount >= 10 && writingExerciseCount >= 10 && listeningExerciseCount >= 10 {
+            if UserDefault.userGotDailyPrize.getString() != wordBrain.getTodayDate() {
+                prizeButton.isEnabled = true
+            }
+        }
+    }
+    
+    private func checkWordCountAndNavigate(controller: UIViewController) {
+        let wordCount = itemArray.count
+        
+        if wordCount < 2 {
+            showAlert(title: "Minimum two words required", message: "") { _ in
+                self.navigationController?.popToRootViewController(animated: true)
+            }
+        } else {
+            DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 0.1){
+                self.navigationController?.pushViewController(controller, animated: true)
+            }
+        }
     }
 }
---
+++ Word Bank/View/TaskView.swift
@@ -0,0 +1,66 @@
+//
+//  TaskView.swift
+//  Word Bank
+//
+//  Created by ibrahim uysal on 17.08.2024.
+//
+
+import UIKit
+
+class TaskView: UIView {
+    
+    private let exerciseCount: Int
+    
+    private lazy var taskButton: UIButton = {
+        let button = UIButton()
+        button.titleLabel?.font = UIFont(name: Fonts.AvenirNextDemiBold, size: 15)
+        button.moveImageTitleLeft()
+        button.isUserInteractionEnabled = false
+        return button
+    }()
+    
+    private let ravenLayerButton: UIButton = {
+       let button = UIButton()
+        button.setButtonCornerRadius(10)
+        button.backgroundColor = Colors.raven
+        button.isUserInteractionEnabled = false
+        return button
+    }()
+    
+    private let blueLayerButton: UIButton = {
+       let button = UIButton()
+        button.setHeight(60)
+        button.setButtonCornerRadius(10)
+        button.backgroundColor = Colors.blue
+        button.isUserInteractionEnabled = false
+        return button
+    }()
+    
+    init(title: String, exerciseCount: Int) {
+        self.exerciseCount = exerciseCount
+        super.init(frame: .zero)
+        
+        taskButton.setTitle(title, for: .normal)
+        
+        addSubview(ravenLayerButton)
+        ravenLayerButton.fillSuperview()
+        
+        addSubview(blueLayerButton)
+        
+        addSubview(taskButton)
+        taskButton.fillSuperview()
+        
+        let image = exerciseCount >= 10 ? Images.check : Images.whiteCircle
+        taskButton.setImageWithRenderingMode(image: image, width: 25, height: 25, color: .white)
+    }
+    
+    override func layoutSubviews() {
+        let width = self.bounds.width
+        let blueLayerWidth = (width/10)*CGFloat(exerciseCount)
+        blueLayerButton.setWidth(blueLayerWidth)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
